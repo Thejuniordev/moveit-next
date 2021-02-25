1c828b7151feeaa15347745be1ef1be14b37808c
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-02-25 15:54:25 -0300

    Add context complete chalenge component

--- src/components/CompletedChallenge.tsx
+++ src/components/CompletedChallenge.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/CompletedChallenge.module.css';
 
 export function CompletedChallenge() {
+    const { challengesCompleted } = useContext(ChallengeContext);
+
     return (
         <div className={styles.completedChallengeContainer}>
             <span>Desafios completos</span>
-            <span>5</span>
+            <span>{ challengesCompleted }</span>
         </div>
     );
 }
